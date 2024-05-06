2c1ef0d0d0803f0aa3f7c6b073279f258d6a1654
김유진 <gracecancho@gmail.com>
2024-05-07 07:26:40 +0900

    싱글톤 테스트

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,25 +18,26 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
-
-    @Bean
-    public static MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
     @Bean
     public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
-                discountPolicy()
-        );
+                discountPolicy());
     }
-
+    @Bean
+    public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
     @Bean
     public DiscountPolicy discountPolicy() {
-        // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
