b140ed9efe4ade5270595a496f9e176785d88871
김유진 <gracecancho@gmail.com>
2024-05-07 12:10:53 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,7 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
 public class NetworkClient {
     private String url;
@@ -13,6 +13,7 @@ public class NetworkClient {
     public void setUrl(String url) {
         this.url = url;
     }
+
 
     //서비스 시작시 호출
     public void connect() {
@@ -28,15 +29,16 @@ public class NetworkClient {
         System.out.println("close + " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disConnect();
     }
-
 }
